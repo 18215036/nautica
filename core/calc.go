78228f946c4d990e0bb8c15199cb7d706a787200
18215036 <18215036.std.stei.itb.ac.id>
2017-11-20 15:54:26 +0700

    Added file buffer

--- core/calc.go
+++ core/calc.go
@@ -6,10 +6,3 @@ package core
 //NO DECO DIVE HANDLING at the moment, will be implemented
 //Add another dive (y/n)?Add another dive (y/n)? //problem? brought from C
 
-type t1 [24]DATA1
-type t2 [16]DATA2
-type t3 [24]DATA3
-
-func dataOne() { //dataOne Initializer
-	
-}
