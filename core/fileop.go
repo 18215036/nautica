596c5a6cabb63d869c41f0e8b86de3e6014d9eae
18215036 <18215036@std.stei.itb.ac.id>
2017-11-25 06:31:56 +0700

    Added SI and RNT Tables Initializers

--- core/fileop.go
+++ core/fileop.go
@@ -11,36 +11,127 @@ import (
     "strings"
 )
 
-func init() { //dataOne Initializer
+func init() { //NDT Table Initializer
     data1, err := readLines("./D1")
     for i := 0; i < 24; i++ {
         dataSlice := strings.Split(data1[i], ",")
         j := 0
-        T1[i].d3.DE = dataSlice[j++]
-        T1[i].NSL = dataSlice[j++]
-        T1[i].d3.d2.A = dataSlice[j++]
-        T1[i].d3.d2.B  = dataSlice[j++]
-        T1[i].d3.d2.C  = dataSlice[j++]
-        T1[i].d3.d2.D  = dataSlice[j++]
-        T1[i].d3.d2.E  = dataSlice[j++]
-        T1[i].d3.d2.F  = dataSlice[j++]
-        T1[i].d3.d2.G  = dataSlice[j++]
-        T1[i].d3.d2.H  = dataSlice[j++]
-        T1[i].d3.d2.I  = dataSlice[j++]
-        T1[i].d3.d2.J  = dataSlice[j++]
-        T1[i].d3.d2.K  = dataSlice[j++]
-        T1[i].d3.d2.L  = dataSlice[j++]
-        T1[i].d3.d2.M  = dataSlice[j++]
-        T1[i].d3.d2.N  = dataSlice[j++]
-        T1[i].d3.d2.O  = dataSlice[j++]
-        T1[i].d3.d2.Z  = dataSlice[j++]
+        T1[i].d3.DE = dataSlice[j]
+        j++
+        T1[i].NSL = dataSlice[j]
+        j++
+        T1[i].d3.d2.A = dataSlice[j]
+        j++
+        T1[i].d3.d2.B  = dataSlice[j]
+        j++
+        T1[i].d3.d2.C  = dataSlice[j]
+        j++
+        T1[i].d3.d2.D  = dataSlice[j]
+        j++
+        T1[i].d3.d2.E  = dataSlice[j]
+        j++
+        T1[i].d3.d2.F  = dataSlice[j]
+        j++
+        T1[i].d3.d2.G  = dataSlice[j]
+        j++
+        T1[i].d3.d2.H  = dataSlice[j]
+        j++
+        T1[i].d3.d2.I  = dataSlice[j]
+        j++
+        T1[i].d3.d2.J  = dataSlice[j]
+        j++
+        T1[i].d3.d2.K  = dataSlice[j]
+        j++
+        T1[i].d3.d2.L  = dataSlice[j]
+        j++
+        T1[i].d3.d2.M  = dataSlice[j]
+        j++
+        T1[i].d3.d2.N  = dataSlice[j]
+        j++
+        T1[i].d3.d2.O  = dataSlice[j]
+        j++
+        T1[i].d3.d2.Z  = dataSlice[j]
     }
 }
 
-func init() { //dataTwo Initializer
+func init() { //SI Table Initializer
+    data2, err := readLines("./D2")
+    for i := 0; i < 16; i++ {
+        dataSlice := strings.Split(data2[i], ",")
+        j := 0
+        T2[i].A = dataSlice[j]
+        j++
+        T2[i].B  = dataSlice[j]
+        j++
+        T2[i].C  = dataSlice[j]
+        j++
+        T2[i].D  = dataSlice[j]
+        j++
+        T2[i].E  = dataSlice[j]
+        j++
+        T2[i].F  = dataSlice[j]
+        j++
+        T2[i].G  = dataSlice[j]
+        j++
+        T2[i].H  = dataSlice[j]
+        j++
+        T2[i].I  = dataSlice[j]
+        j++
+        T2[i].J  = dataSlice[j]
+        j++
+        T2[i].K  = dataSlice[j]
+        j++
+        T2[i].L  = dataSlice[j]
+        j++
+        T2[i].M  = dataSlice[j]
+        j++
+        T2[i].N  = dataSlice[j]
+        j++
+        T2[i].O  = dataSlice[j]
+        j++
+        T2[i].Z  = dataSlice[j]
+    }
 }
 
-func init() { //dataThree Initializer
+func init() { //RNT Table Initializer
+    data3, err := readLines("./D3")
+    for i := 0; i < 24; i++ {
+        dataSlice := strings.Split(data3[i], ",")
+        j := 0
+        T3[i].DE = dataSlice[j]
+        j++
+        T3[i].d2.Z = dataSlice[j]
+        j++
+        T3[i].d2.O  = dataSlice[j]
+        j++
+        T3[i].d2.N  = dataSlice[j]
+        j++
+        T3[i].d2.M  = dataSlice[j]
+        j++
+        T3[i].d2.L  = dataSlice[j]
+        j++
+        T3[i].d2.K  = dataSlice[j]
+        j++
+        T3[i].d2.J  = dataSlice[j]
+        j++
+        T3[i].d2.I  = dataSlice[j]
+        j++
+        T3[i].d2.H  = dataSlice[j]
+        j++
+        T3[i].d2.G  = dataSlice[j]
+        j++
+        T3[i].d2.F  = dataSlice[j]
+        j++
+        T3[i].d2.E  = dataSlice[j]
+        j++
+        T3[i].d2.D  = dataSlice[j]
+        j++
+        T3[i].d2.C  = dataSlice[j]
+        j++
+        T3[i].d2.B  = dataSlice[j]
+        j++
+        T3[i].d2.A  = dataSlice[j]
+    }
 }
 
 // readLines reads a whole file into memory
@@ -74,5 +165,3 @@ func writeLines(lines []string, path string) error {
     }
     return w.Flush()
 }
-
-func parseTables (path string) []
