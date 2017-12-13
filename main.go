7adcedd7fff025998de2d747beb7f5f2edc1d253
18215036 <18215036@std.stei.itb.ac.id>
2017-12-13 07:04:18 +0700

    Added imported packages, remote address as id, write to file

--- main.go
+++ main.go
@@ -1,35 +1,31 @@
+//18215036/Ahmad Fawwaz Zuhdi
 package main
 
-import {
-	"fmt"
+import (
 	"log"
 	"net/http"
+	"encoding/json"
 
 	"nautica/core"
 
 	"github.com/gorilla/mux"
-}
+)
 
 func CreateDiveSequence(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var sequence DiveSeq
+	var sequence core.DiveSeq
 	var unparsed string
 	_ = json.NewDecoder(r.Body).Decode(&sequence)
-	sequence.ID = params["id"]
-	sequence, unparsed = Calculate(sequence)
-	writeLines(unparsed, ./DiveLog)
+	sequence.ID = (*r).RemoteAddr
+	sequence, unparsed = core.Calculate(sequence)
+	note := []string{unparsed}
+	core.WriteLines(note, "./DiveLog")
 	json.NewEncoder(w).Encode(sequence)
 }
 
-/*
-func GetDiveSequence(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(diveSeq)
-}
-*/
-
 func main() {
+	addr := ":8081"
 	router := mux.NewRouter()
 	router.HandleFunc("/dives/{id}", CreateDiveSequence).Methods("POST")
-	//router.HandleFunc("/dives/{id}", GetDiveSequence).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8081", router))
+	log.Println("nautica running on localhost", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
 }
